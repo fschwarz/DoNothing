c0a80ad8a1df3f23f1698dd196e83a468792d9b3
fschwarz <32054208+fschwarz@users.noreply.github.com>
2026-04-11 19:45:17 +0200

    Background image

--- Assets/_PROJECT/Scripts/DopamineVFXManager.cs
+++ Assets/_PROJECT/Scripts/DopamineVFXManager.cs
@@ -1,16 +1,33 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class DopamineVFXManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private static readonly int BaseLevel = Shader.PropertyToID("_BaseLevel");
+
+    [Range(0f,1f)]
+    public float dopamine;
+
+    public Renderer dopamineBar;
+    public Volume postProcessingVolume;
+    public AnimationCurve saturationCurve;
+    private ColorAdjustments colorAdjustments;
+    public Transform hand;
+    public float shakingFrequency = 1;
+    public float shakingIntensity = 1;
     void Start()
     {
-        
+        postProcessingVolume.profile.TryGet<ColorAdjustments>(out colorAdjustments);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        colorAdjustments.saturation.value = saturationCurve.Evaluate(dopamine)*200-100;
+        dopamineBar.material.SetFloat(BaseLevel,dopamine);
+        hand.localPosition = new Vector3(Mathf.PerlinNoise(Time.time*shakingFrequency+2,dopamine+1.2125f),Mathf.PerlinNoise(Time.time*shakingFrequency,dopamine)) * (dopamine * 3 * shakingIntensity);
+        hand.rotation = Quaternion.Euler(new Vector3(hand.rotation.eulerAngles.x,hand.rotation.eulerAngles.y,Mathf.PerlinNoise(Time.time*dopamine*0.01f,dopamine)));
         
     }
 }
